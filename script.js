0b43039f9711b1d6b03265cb0d3103d237673e71
yasuaki <yasuaki@MBP.local>
2013-08-25 12:27:41 +0900

    対応サイト追加

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 $(function(){
-  location.href=$("a#pickup").attr("href");
+  $(document).ready(function(){
+    var current_url = window.location.href;
+    var redirect_url;
+
+    if(current_url.indexOf("http://2ch-c.net/",0) != -1){
+      redirect_url = $("a#pickup").attr("href");
+    }else if(current_url.indexOf("http://matometatta-news.net",0) != -1){
+      redirect_url = $("div.title > a").attr("href");
+    }else if(current_url.indexOf("http://newmofu.doorblog.jp/",0) != -1){
+      redirect_url = $("div.title_link > a").attr("href");
+    }else if(current_url.indexOf("http://newser.cc/",0) != -1){
+      redirect_url = $("td.news-link > a").attr("href");
+    }else if(current_url.indexOf("http://newpuru.doorblog.jp/",0) != -1){
+      redirect_url = $("a.titleLink").attr("href");
+    }else if(current_url.indexOf("http://matomeantena.com/feed",0) != -1){
+      redirect_url = $("li.rss_link > a").attr("href");
+    }
+    if(redirect_url !== undefined){
+      location.href=redirect_url;
+    }
+  });
 });
